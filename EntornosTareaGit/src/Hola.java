57bd2c258695e81f32fe25bcf5ce5c0df440f1cf
MarioBV0 <130754686+MarioBV0@users.noreply.github.com>
2023-04-27 09:54:20 +0200

    Update Hola.java

--- EntornosTareaGit/src/Hola.java
+++ EntornosTareaGit/src/Hola.java
@@ -3,7 +3,8 @@ public class Hola {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Esto es un ejempl");
+		System.out.println("Esto es un ejemplo");
+		
 		
 	}
 
